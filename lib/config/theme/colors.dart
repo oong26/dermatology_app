0e3b1f1a27cf4871956b22fd2e7b6a8a24b5eadd
oong26 <37205723+oong26@users.noreply.github.com>
2024-09-05 11:28:41 +0700

    ui : setup project

---
+++ lib/config/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const mPrimaryColor = Color(0XFF2260FF);
+const mSecondaryColor = Color(0XFFCAD6FF);
+const mPrimaryTextColor = Color(0XFF2260FF);
+const mSecondaryTextColor = Color(0XFF000000);
+const mSuccessColor = Color(0XFF00A773);
+const mErrorColor = Color(0XFFFF5963);
+const mWarningColor = Color(0XFFF9CF58);
+const mInfoColor = Color(0XFFFFFFFF);
